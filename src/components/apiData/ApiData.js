121e53c4d371b556e307cc29ce15a34c0ad2e652
Alejandro Bahena <alejandro.bahsan@gmail.com>
2023-05-23 23:43:01 -0400

    Portfolio cards v5

--- src/components/apiData/ApiData.js
+++ src/components/apiData/ApiData.js
@@ -5,12 +5,6 @@ const ApiData = ({ limit }) => {
   //Overflow: The description of each project is shown by an overflow animation and a popup.
   const [buttonPopup, setButtonPopup] = useState(false);
 
-  useEffect(() => {
-    buttonPopup
-      ? (document.body.style.overflow = "hidden")
-      : (document.body.style.overflow = "visible");
-  }, [buttonPopup]);
-
   // ____________________________________________________________________________________
 
   // Capitalized the first letter of a word and prevents any other letter being capitalized.
@@ -27,13 +21,12 @@ const ApiData = ({ limit }) => {
       .catch((err) => console.log(err));
   }, []);
 
-  const [clickHandler, setClickHandler] = useState(12345);
+  const [clickHandler, setClickHandler] = useState(1234);
 
   const handleClick = (state, e) => {
     setButtonPopup(state);
-    setClickHandler(e.target.id);
-    console.log(clickHandler);
-    return e.target.id;
+
+    return setClickHandler(e.target.id);
   };
 
   //Reference source https://bobbyhadz.com/blog/react-foreach.
@@ -41,51 +34,57 @@ const ApiData = ({ limit }) => {
   //The method filter and map, are used to populated with the results obtained after the forEach fuction on every element found in the iteration.
   //We return all the information, and add the new objects for each index created by the iteration to the empty array(render_api_data_JSX) with the push method.
   const render_api_data_JSX = [];
-  const limitData = limit.map((el) => el.id);
+  const limitData = limit.map((element) => element.id);
 
   limitData.forEach((element) => {
     gitData
-      .filter((x) => x.id === element)
-      .map((list) => {
+      .filter((projects) => projects.id === element)
+      .map((projectList) => {
         return render_api_data_JSX.push(
           <>
             {/* - */}
             <div
-              key={list.id}
-              id={list.id}
+              key={projectList.id}
+              id={projectList.id}
               className="col-12 col-md-6 col-lg-4 portfolio-card"
             >
-              {list ? (
+              {projectList ? (
                 <>
                   <img
-                    src={"/assets/projects-img/" + list.name + ".jpg"}
+                    src={"/assets/projects-img/" + projectList.name + ".jpg"}
                     alt=" project"
                     className="card-img-top"
                   />
-                  <div className="portfolio-content ">
-                    <h5 className="card-title">
-                      {list.name
-                        ? //Removes the hypens and undercores from the name of each project obtained from the Github API
-                          list.name.replace(/[_-]/g, " ")
-                        : list.name}
-                    </h5>
-                    <br></br>
-                    <p className="portfolio-description">
-                      Built with:{" "}
-                      {list.topics
-                        ? //Capitalized the first letter of the name of each tecnology applied on every project obtained from the Github API
-                          list.topics.map(capitalizedFirstLetter).join(", ")
-                        : list.topics}
-                      <br></br>
-                      <a
-                        className="read-more"
-                        onClick={(e) => handleClick(true, e)}
-                        id={list.id}
-                      >
-                        Read more
-                      </a>
-                    </p>
-                  </div>
+                  {!buttonPopup && (
+                    <>
+                      <div className={"portfolio-content"}>
+                        <h5 className="card-title">
+                          {projectList.name
+                            ? //Removes the hypens and undercores from the name of each project obtained from the Github API
+                              projectList.name.replace(/[_-]/g, " ")
+                            : projectList.name}
+                        </h5>
+                        <br></br>
+                        <p className="portfolio-description">
+                          Built with:{" "}
+                          {projectList.topics
+                            ? //Capitalized the first letter of the name of each tecnology applied on every project obtained from the Github API
+                              projectList.topics
+                                .map(capitalizedFirstLetter)
+                                .join(", ")
+                            : projectList.topics}
+                          <br></br>
+                          <a
+                            className="read-more"
+                            onClick={(e) => handleClick(true, e)}
+                            id={projectList.id}
+                          >
+                            Read more
+                          </a>
+                        </p>
+                      </div>
+                    </>
+                  )}
                 </>
               ) : (
                 <div className="portfolio-content ">
@@ -95,7 +94,7 @@ const ApiData = ({ limit }) => {
               <Popupbox
                 trigger={buttonPopup}
                 setTrigger={setButtonPopup}
-                list={list}
+                gitData={gitData}
                 idClicked={clickHandler}
               >
                 {" "}
--- src/components/popup-component/Popupbox.js
+++ src/components/popup-component/Popupbox.js
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Popupbox = ({ trigger, setTrigger, list, clickHandler }) => {
-  console.log(clickHandler);
+const Popupbox = ({ trigger, setTrigger, gitData, idClicked }) => {
+  const [repoId, setRepoId] = useState([]);
+  const capitalizedFirstLetter = (str) =>
+    str[0].toUpperCase() + str.slice(1).toLowerCase();
+
+  useEffect(() => {
+    trigger
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "visible");
+
+    const idClickedInt = parseInt(idClicked);
+    const gitDataArr = gitData.map((data) => data);
+    console.log(gitDataArr);
+    let projectId = gitDataArr.filter(
+      (projects) => projects.id === idClickedInt
+    );
+    setRepoId(projectId[0]);
+  }, [trigger]);
+
   return trigger ? (
-    <div className="popup">
+    <div onClick={() => setTrigger(false)} className="popup">
       <div className="popup-inner">
-        <img
-          src={"/assets/projects-img/" + list.name + ".jpg"}
-          alt="popup-img "
-          className="popup-img"
-        />
-        {list.name
-          ? //Removes the hypens and undercores from the name of each project obtained from the Github API
-            list.name.replace(/[_-]/g, " ")
-          : list.name}
-        <button className="close-btn" onClick={() => setTrigger(false)}>
-          close
-        </button>
+        {repoId && (
+          <>
+            <img
+              src={"/assets/projects-img/" + repoId.name + ".jpg"}
+              alt="popup-img "
+              className="popup-img"
+            />
+            <h4>{repoId.name.replace(/[_-]/g, " ")}</h4>
+            <h5>
+              {"Built with: " +
+                //Capitalized the first letter of the name of each tecnology applied on every project got from the Github API
+                repoId.topics.map(capitalizedFirstLetter).join(", ")}
+            </h5>
+            <h6>{repoId.description}</h6>
+          </>
+        )}
+        <button
+          type="button"
+          className="close-btn "
+          aria-label="Close"
+        ></button>
         {trigger.children}
       </div>
     </div>
