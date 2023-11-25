ac623ac2f850c05b827e02948179f79e76040f25
Alejandro Bahena <alejandro.bahsan@gmail.com>
2023-11-25 18:26:08 -0500

    URL Modification done.

--- src/router/Routing.js
+++ src/router/Routing.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Routes, Route, HashRouter, Navigate } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import ContactForm from "../components/contact-component/ContactForm";
 import Main from "../components/Main";
 
 const Routing = () => {
   return (
-    <HashRouter>
-      {/*Main content */}
+    <BrowserRouter>
+      {/* Main content */}
       <section className="content">
         <Routes>
           {/* Navigate will be used in order to be redirected to the main website */}
@@ -17,16 +17,13 @@ const Routing = () => {
             path="/*"
             element={
               <div className="404-page">
-                {" "}
-                <h1 className="head">Error 404 - Page not found</h1>{" "}
+                <h1 className="head">Error 404 - Page not found</h1>
               </div>
             }
           />
         </Routes>
       </section>
-
-      {/* Footer */}
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
