db0a750613c78aad5a9a7d336574eaddb4df6fa9
Alejandro Bahena <alejandro.bahsan@gmail.com>
2023-11-25 18:37:59 -0500

    Routing and public>404.html annotations.

--- src/router/Routing.js
+++ src/router/Routing.js
@@ -5,14 +5,22 @@ import Main from "../components/Main";
 
 const Routing = () => {
   return (
+    // Use BrowserRouter for cleaner URLs
     <BrowserRouter>
       {/* Main content */}
       <section className="content">
+        {/* Define routes within a Routes component */}
         <Routes>
-          {/* Navigate will be used in order to be redirected to the main website */}
+          {/* Use Navigate to redirect from the root path to "/main" */}
           <Route path="/" element={<Navigate to="/main" />} />
+          {/* Render Main component when the path is "/main" */}
           <Route path="/main" element={<Main />} />
+          {/* Render ContactForm component when the path is "/contact" */}
           <Route path="/contact" element={<ContactForm />} />
+          {/* 
+            Render a 404 page for any other paths not matched.
+            This is a catch-all route.
+          */}
           <Route
             path="/*"
             element={
